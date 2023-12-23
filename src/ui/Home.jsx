b3a951c325bf516cce4fbfa8f6b28834e46b54a1
dumialex32 <dumitrasalexandru26@gmail.com>
2023-12-23 13:58:55 +0100

    third secondary styling commit

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,15 +1,17 @@
 function Home() {
   return (
-    <div className="py-5">
-      <h1 className="text-xl text-yellow-500 font-semibold text-center mb-7">
-        <span className="text-stone-700">The best pizza.</span>
-        <br />
-        Straight out of the oven, straight to you.
-      </h1>
+    <div className="max-w-2xl">
+      <div className="py-5">
+        <h1 className="text-xl text-yellow-500 font-semibold text-center mb-7">
+          <span className="text-stone-700">The best pizza.</span>
+          <br />
+          Straight out of the oven, straight to you.
+        </h1>
 
-      <div className="text-center">
-        <h2>Welcome, please tell us your name</h2>
-        <input type="text" className="input"></input>
+        <div className="text-center">
+          <h2>Welcome, please tell us your name</h2>
+          <input type="text" className="input"></input>
+        </div>
       </div>
     </div>
   );
